ff18ecd38962e318df13713febfb9d1e9c2f17f4
Xander <alexander.eagle@ssfs.org>
2020-01-07 10:15:58 -0500

    Initial commit

---
+++ brick.py
@@ -0,0 +1,19 @@
+import pygame
+
+class Brick(pygame.sprite.Sprite):
+
+    def __init__(self, width, height, color):
+        # initialize sprite super class
+
+        # finish setting the class variables to the parameters
+        self.width = width
+        self.height = height
+        self.color = color
+
+        # Create a surface with the correct height and width
+        self.image =
+
+        # Get the rect coordinates
+        self.rect =
+
+        # Fill the surface with the correct color
